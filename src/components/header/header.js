56e9c97aa6a8d763732454cdde0b40fd79b681da
Ivan Krasilnikov <bob.cody.in@gmail.com>
2019-02-10 04:09:28 +0300

    Update Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,41 +1,100 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { toggleFilter, toggleMenu } from "../../redux/actions";
 
+// Configs
+import routes from "../../configs/routes";
+
+// Images
 import logo from "../../images/logo@2x.png";
+import logoInvert from "../../images/logoInvert@2x.png";
 
-import { Link } from "react-router-dom";
+// Components
+import Navigation from "../navigation/navigation";
 
 class Header extends React.Component {
-  static propTypes = {};
+  static propTypes = {
+    isFilterShow: PropTypes.bool,
+    isMenuShow: PropTypes.bool
+  };
+
+  // ;;events --------------------------------------------------------------------------------------
+
+  handleFilterButtonClick = () => {
+    this.props.dispatch(toggleFilter());
+  };
+
+  handleMenuButtonClick = () => {
+    this.props.dispatch(toggleMenu());
+  };
+
+  // ;;compute -------------------------------------------------------------------------------------
+
+  getLogoSrc() {
+    return this.props.isMenuShow ? logoInvert : logo;
+  }
+
+  // ;;render --------------------------------------------------------------------------------------
+
+  renderFilterButton() {
+    if (this.props.location.pathname !== routes.jogs) return null;
+
+    return (
+      <button
+        className={classNames({
+          "header__filter-button": true,
+          "header__filter-button_active": this.props.isFilterShow
+        })}
+        onClick={this.handleFilterButtonClick}
+        type="button"
+      />
+    );
+  }
+
+  renderMenuButton() {
+    return (
+      <button
+        className={classNames({
+          "header__menu-button": true,
+          "header__menu-button_active": this.props.isMenuShow
+        })}
+        onClick={this.handleMenuButtonClick}
+        type="button"
+      />
+    );
+  }
 
   render() {
     return (
-      <header className="header">
-        <Link to="/" className="header__logo">
-          <img src={logo} alt="logo" />
+      <header
+        className={classNames({
+          header: true,
+          "header_menu-popup-show": this.props.isMenuShow
+        })}
+      >
+        <Link to={routes.jogs} className="header__logo">
+          <img
+            className="header__logo-img"
+            src={this.getLogoSrc()}
+            alt="logo"
+          />
         </Link>
-        <nav className="header__nav">
-          <ul className="header__nav-list">
-            <li className="header__nav-list-item">
-              <Link to="/" className="header__nav-list-link">
-                Jogs
-              </Link>
-            </li>
-            <li className="header__nav-list-item">
-              <Link to="/" className="header__nav-list-link">
-                Info
-              </Link>
-            </li>
-            <li className="header__nav-list-item">
-              <Link to="/" className="header__nav-list-link">
-                Contact us
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <div className="header__controls">
+          {this.renderFilterButton()}
+          {this.renderMenuButton()}
+          <Navigation />
+        </div>
       </header>
     );
   }
 }
 
-export default Header;
+const PreparedHeader = connect(state => ({
+  isFilterShow: state.filter.show,
+  isMenuShow: state.menu.show
+}))(Header);
+
+export default withRouter(PreparedHeader);
