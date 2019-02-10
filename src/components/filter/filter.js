b1d6254431f47cec890ad7d1ba75aa881c9a0969
Ivan Krasilnikov <bob.cody.in@gmail.com>
2019-02-10 19:31:23 +0300

    Update Filter

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,22 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 class Filter extends React.Component {
-
   static propTypes = {};
 
-  render() {
+  state = {
+    dateFrom: "",
+    dateTo: ""
+  };
 
+  // ;;events --------------------------------------------------------------------------------------
+
+  handleDateFromChange = e => {
+    this.changeInputValue("dateFrom", e.target.value);
+  };
+
+  handleDateToChange = e => {
+    this.changeInputValue("dateTo", e.target.value);
+  };
+
+  // ;;inner ---------------------------------------------------------------------------------------
+
+  changeInputValue(name, value) {
+    this.setState({
+      [name]: value
+    });
+  }
+
+  // ;;render --------------------------------------------------------------------------------------
+
+  render() {
     return (
       <div className="filter">
         <div className="filter__content">
           <label className="filter__field">
             <span className="filter__label">Date from</span>
-            <input className="filter__input" type="date" />
+            <input
+              className="filter__input"
+              type="date"
+              value={this.state.dateFrom}
+              onChange={this.handleDateFromChange}
+            />
           </label>
           <label className="filter__field">
             <span className="filter__label">Date to</span>
-            <input className="filter__input" type="date" />
+            <input
+              className="filter__input"
+              type="date"
+              value={this.state.dateTo}
+              onChange={this.handleDateToChange}
+            />
           </label>
         </div>
       </div>
