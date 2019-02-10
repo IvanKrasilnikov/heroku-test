e895bc036f425c3d9f31ed4dbe5f7f1748134b4d
Ivan Krasilnikov <bob.cody.in@gmail.com>
2019-02-10 17:07:40 +0300

    Add styles for JogsEmpty

--- src/components/jogs-empty/jogs-empty.js
+++ src/components/jogs-empty/jogs-empty.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../button/button';
 
-import jogSad from '../../images/jogSad@2x.png';
-
 class JogsEmpty extends React.PureComponent {
 
   static propTypes = {
@@ -22,7 +20,6 @@ class JogsEmpty extends React.PureComponent {
 
     return (
       <div className="jogs-empty">
-        <img src={jogSad} alt="sad face" />
         <span className="jogs-empty__title">Nothing is there</span>
         <Button handleClick={this.handleButtonClick}>Create your jog first</Button>
       </div>
